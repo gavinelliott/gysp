c937201294ba44bb7d0edeb226fa48f8a3c47750
Gavin Elliott <gavinelliott@admins-MacBook-Pro-5.local>
2015-11-15 21:49:17 +0000

    Latest Prototype

--- app/routes/iteration3-v2.js
+++ app/routes/iteration3-v2.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 
 router.post('/start_smart', function (req, res) {
-  if(req.body["ni_number"].replace( /\s/g, "") == "1234567843218765"){  res.redirect("work") }else{
+  if(req.body["ni_number"].replace( /\s/g, "") == "1234567843218765"){  res.redirect("declaration") }else{
     res.redirect("start_smart_error")
     }
 });
@@ -23,7 +23,7 @@ router.get('/question2', function (req, res) {
 
 switch(req.query["radio-group"]) {
   case "Never been married":
-    res.render("iteration3-v2/calculated");
+    res.render("iteration3-v2/tel-number");
   case "Married":
     res.render("iteration3-v2/year-of-marriage");
   case "Other":
@@ -37,7 +37,7 @@ switch(req.query["radio-group"]) {
 router.get('/marriage-year', function (req, res) {
 switch(req.query["marriage_year"]) {
   case "1973":
-    res.redirect("calculated");
+    res.redirect("tel-number");
   default:
     //None of the above, go to error
     res.redirect("error");
