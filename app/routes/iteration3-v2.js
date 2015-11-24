e9e031bc3590ff97dbfe0487298f296f9dfb7e83
Colin Oakley <colin@htmlandbacon.com>
2015-11-24 09:25:23 +0000

    fixed routing error

--- app/routes/iteration3-v2.js
+++ app/routes/iteration3-v2.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 router.post('/start_smart', function (req, res) {
   if(req.body["ni_number"].replace( /\s/g, "") == "1234567843218765"){  res.redirect("declaration") }else{
-    res.redirect("start_smart_error")
+    res.redirect("start_smart_error");
     }
 });
 
@@ -17,25 +17,31 @@ router.get('/overseas', function (req, res) {
 router.get('/question2', function (req, res) {
 
     if(req.query["radio-inline-group"]== "Yes"){
-        res.redirect("error")
+        res.redirect("error");
     }else if(req.query["radio-inline-group"]== "No"){
-    res.render('iteration3-v2/question1');}
+        res.render('iteration3-v2/question1');}else{
 
 switch(req.query["radio-group"]) {
   case "Never been married":
     res.render("iteration3-v2/tel-number");
+    break;
   case "Married":
     res.render("iteration3-v2/year-of-marriage");
+    break;
   case "Civil":
     res.render("iteration3-v2/year-of-civil");
+    break;
   case "Other":
     res.render("iteration3-v2/error");
+    break;
   case "Divorced":
     res.render("iteration3-v2/tel-number");
+    break;
   default:
     //None of the above, go to error
     res.redirect("error");
   }
+}
 });
 
 router.get('/marriage-year', function (req, res) {
@@ -47,7 +53,7 @@ switch(req.query["marriage_year"]) {
     res.redirect("error");
     }
   });
-  
+
   router.get('/civil-year', function (req, res) {
 switch(req.query["civil_year"]) {
   case "1973":
