7a884b13a6a83ac38de2e80d420275429b78ebce
Colin Oakley <colin@htmlandbacon.com>
2015-12-12 14:34:32 +0000

    update beta to have second code route

--- app/routes/beta.js
+++ app/routes/beta.js
@@ -87,8 +87,8 @@ router.post('/secure', function(req, res) {
 
   res.locals.error = true;
 
-  if (req.body.reference.replace(/\s/g, "") === "1234567843218765" &&
-    (
+  if ((req.body.reference.replace(/\s/g, "") === "1234567843218765" || req.body.reference.replace(/\s/g, "").toLowerCase() === "qwx5ychpnrjv") &&
+  (
       (req.body.value3 === undefined || req.body.value3 == "1") &&
       (req.body.value4 === undefined || req.body.value4 == "2") &&
       (req.body.value5 === undefined || req.body.value5 == "3") &&
