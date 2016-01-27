eb05cbec3ec9ef27418f75283a987c82d65496c2
Craig Abbott <craigabbott.digitaldwp@gmail.com>
2016-01-27 15:13:12 +0000

    Added function to keep submition date current on end page

--- app/routes/january.js
+++ app/routes/january.js
@@ -215,13 +215,45 @@ router.get('/unhappy-ending', function(req, res) {
 });
 
 router.get('/end', function(req, res) {
-      res.render('january/end');
-  // if (req.session.figure === false) {
-  //   res.render('january/end-no-figure');
-  // } else {
-  //   res.render('january/end');
-  // }
+  var completeDate = get_todays_date();
+
+  res.render('january/end', {completeDate: completeDate});
 });
+
+function get_todays_date() {
+  var date    = new Date(),
+      monthNames = [
+        "January", "February", "March",
+        "April", "May", "June", "July",
+        "August", "September", "October",
+        "November", "December"
+      ],
+      day     = date.getDate(),
+      month   = date.getMonth(),
+      year    = date.getFullYear(),
+      suffix  = get_nth_suffix(day);
+      date = day + suffix + ' ' + monthNames[month] + ' ' + year;
+
+  return date;
+
+  function get_nth_suffix(day) {
+    switch (day) {
+      case 1:
+      case 21:
+      case 31:
+        return 'st';
+      case 2:
+      case 22:
+        return 'nd';
+      case 3:
+      case 23:
+        return 'rd';
+      default:
+        return 'th';
+    }
+  }
+}
+
 
 router.get('/reset', function(req, res) {
 
