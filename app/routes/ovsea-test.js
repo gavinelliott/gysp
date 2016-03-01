79c2595237f5e43d6272072a84b6a12fbfb75b48
Craig Abbott <craigabbott.digitaldwp@gmail.com>
2016-03-01 11:25:25 +0000

    Added demo for overseas journey

--- app/routes/ovsea-test.js
+++ app/routes/ovsea-test.js
@@ -14,31 +14,58 @@ router.post('/have-you-lived-abroad', function(req, res) {
   }
 });
 
+// Have you worked abroad?
+router.get('/have-you-worked-abroad', function(req, res) {
+  res.render('ovsea-test/have-you-worked-abroad');
+});
+
+router.post('/have-you-worked-abroad', function(req, res) {
+  if ( req.body['worked-outside-select'] === 'Yes' ) {
+    res.redirect('what-countries-have-you-worked-in');
+  } else {
+    res.redirect('/');
+  }
+});
+
 // Which countries have you lived in?
 router.get('/what-countries-have-you-lived-in', function(req, res) {
   res.render('ovsea-test/what-countries-have-you-lived-in');
 });
 
 router.post('/what-countries-have-you-lived-in', function(req, res) {
-  res.redirect('tell-us-about');
+  var countries = [];
+
+  for ( var country in req.body ) {
+    countries.push(req.body[country]);
+  }
+  res.cookie('c-lived-count', countries.length);
+  res.cookie('c-lived-list', countries);
+
+  res.redirect('tell-us-about-lived');
 });
 
 // Tell us about the country?
-router.get('/tell-us-about', function(req, res) {
-  res.render('ovsea-test/tell-us-about');
+router.get('/tell-us-about-lived', function(req, res) {
+  var countries = req.cookies['c-lived-list'];
+  var country = countries.shift();
+
+  res.render('ovsea-test/tell-us-about-lived', {country: country});
 });
 
-router.post('/tell-us-about', function(req, res) {
-  res.redirect('have-you-worked-anywhere-else');
-});
+router.post('/tell-us-about-lived', function(req, res) {
+  var countries = req.cookies['c-lived-list'];
+  var country = '';
+  if ( countries !== undefined ) {
+    country = countries.shift();
+  }
+  res.cookie('c-lived-count', countries.length);
+  res.cookie('c-lived-list', countries);
 
-// Have you worked abroad?
-router.get('/have-you-worked-anywhere-else', function(req, res) {
-  res.render('ovsea-test/have-you-worked-anywhere-else');
-});
-
-router.post('/have-you-worked-anywhere-else', function(req, res) {
-  res.redirect('/');
+  if ( countries.length > 0 ) {
+    res.redirect('tell-us-about-lived');
+  } else {
+    res.redirect('have-you-worked-anywhere-else');
+  }
 });
 
 // Have you worked anywhere else?
@@ -47,7 +74,61 @@ router.get('/have-you-worked-anywhere-else', function(req, res) {
 });
 
 router.post('/have-you-worked-anywhere-else', function(req, res) {
-  res.redirect('/');
+  if ( req.body['worked-outside-select'] === 'Yes' ) {
+    res.redirect('what-countries-have-you-worked-in');
+  } else {
+    res.redirect('/');
+  }
+});
+
+// Which countries have you worked in?
+router.get('/what-countries-have-you-worked-in', function(req, res) {
+  res.render('ovsea-test/what-countries-have-you-worked-in');
+});
+
+router.post('/what-countries-have-you-worked-in', function(req, res) {
+  var countries = [];
+
+  for ( var country in req.body ) {
+    countries.push(req.body[country]);
+  }
+  res.cookie('c-worked-count', countries.length);
+  res.cookie('c-worked-list', countries);
+
+  res.redirect('tell-us-about-worked');
+});
+
+// Tell us about the country you worked in?
+router.get('/tell-us-about-worked', function(req, res) {
+  var countries = req.cookies['c-worked-list'];
+  var country = '';
+  if ( countries !== undefined ) {
+    country = countries.shift();
+  }
+
+  res.render('ovsea-test/tell-us-about-worked', {country: country});
+  });
+
+router.post('/tell-us-about-worked', function(req, res) {
+  var countries = req.cookies['c-worked-list'];
+  var country = '';
+  if ( countries !== undefined ) {
+    country = countries.shift();
+  }
+
+  res.cookie('c-worked-count', countries.length);
+  res.cookie('c-worked-list', countries);
+
+  if ( countries.length > 0 ) {
+    res.redirect('tell-us-about-worked');
+  } else {
+    for (var cookie in req.cookies) {
+      if ( cookie !== 'seen_cookie_message' ) {
+        res.clearCookie(cookie);
+      }
+    }
+    res.redirect('/');
+  }
 });
 
 
