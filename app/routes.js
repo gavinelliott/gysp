4368319133582bef4cd16039d261e38292bdbe4f
beckysarahlee <beckylee@admins-MacBook-Pro-5.local>
2017-08-10 15:08:19 +0100

    descriptions on index page, research notes added

--- app/routes.js
+++ app/routes.js
@@ -31,6 +31,7 @@ router.use("/sprint9v3", require('./routes/sprint9v3.js'));
 router.use("/sprint9v4", require('./routes/sprint9v4.js'));
 router.use("/sprint9v5", require('./routes/sprint9v5.js'));
 router.use("/sprint10", require('./routes/sprint10.js'));
+router.use("/sprint10v2", require('./routes/sprint10v2.js'));
 router.use("/mvpdob", require('./routes/mvpdob.js'));
 router.use("/mvp", require('./routes/mvp.js'));
 
