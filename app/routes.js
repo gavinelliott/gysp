303bbbb682975d259ac77ba0729accdbf233a2b4
beckysarahlee <beckylee@admins-MacBook-Pro-5.local>
2017-08-22 14:58:37 +0100

    Northern Ireland prototype

--- app/routes.js
+++ app/routes.js
@@ -34,6 +34,7 @@ router.use("/sprint10", require('./routes/sprint10.js'));
 router.use("/sprint10v2", require('./routes/sprint10v2.js'));
 router.use("/mvpdob", require('./routes/mvpdob.js'));
 router.use("/mvp", require('./routes/mvp.js'));
+router.use("/northernireland", require('./routes/northernireland.js'));
 
 
 module.exports = router;
