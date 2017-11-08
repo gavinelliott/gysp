52df5058904df73ce1903518273cba9bd9f51301
beckysarahlee <beckylee@Beckys-Macbook-Pro.local>
2017-11-08 14:28:44 +0000

    Date of claim content changes

--- app/routes.js
+++ app/routes.js
@@ -34,6 +34,7 @@ router.use("/sprint10", require('./routes/sprint10.js'));
 router.use("/sprint10v2", require('./routes/sprint10v2.js'));
 router.use("/sprint11", require('./routes/sprint11.js'));
 router.use("/sprint12", require('./routes/sprint12.js'));
+router.use("/sprint12v2", require('./routes/sprint12v2.js'));
 router.use("/mvpdob", require('./routes/mvpdob.js'));
 router.use("/mvp", require('./routes/mvp.js'));
 router.use("/northernireland", require('./routes/northernireland.js'));
